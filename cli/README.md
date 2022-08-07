f1fcdcc1e4464246c9cb3077bef3508db275c7d3
Yang Hau <yuanyanghau@gmail.com>
2022-08-08 01:39:50 +0700

    feat: Enable read keys by yaml

--- cli/README.md
+++ cli/README.md
@@ -7,3 +7,12 @@ $dualbox enc --file0 <file_path_0> --file1 <file_path_1> --key0 <key_0> --key1 <
 $dualbox enc --file0 <file_path_0> --file1 <file_path_1> --key-file <key_file_path>
 $dualbox dec --file <file_path> --key <key>
 ```
+
+### key file example
+```yaml
+keys: 
+    # for file 0
+    - f79681852aad0428a5005a4dc0c25404bb3c3c2b387410a53cf6253e09e416db
+    # for file 1
+    - 3aaf57cdd0fc902048388f7ebf9fe4f175ca3182c23109734e97323b5d719ab7
+```
--- cli/enc.go
+++ cli/enc.go
@@ -6,19 +6,30 @@ import (
 	"dualbox/pack"
 	"encoding/hex"
 	"fmt"
+	"io/ioutil"
+	"os"
 	"time"
 
 	"github.com/urfave/cli/v2"
+	"gopkg.in/yaml.v3"
 )
 
 func Enc() *cli.Command {
 	var filePaths [2]string
 	var keys [2]string
+	var keyFilePath string
 	var output string
 	return &cli.Command{
 		Name:  "enc",
 		Usage: "encrypt files with given keys",
 		Action: func(ctx *cli.Context) error {
+			if keyFilePath != "" {
+				_keys, err := getKeysFromKeyFile(keyFilePath)
+				if err != nil {
+					return fmt.Errorf("failed to get keys from key file: %w", err)
+				}
+				copy(keys[:], _keys)
+			}
 			return enc(filePaths[:], keys[:], output)
 		},
 		Flags: []cli.Flag{
@@ -41,14 +52,18 @@ func Enc() *cli.Command {
 				Aliases:     []string{"k0"},
 				Usage:       "encryption key with index 0",
 				Destination: &keys[0],
-				Required:    true,
 			},
 			&cli.StringFlag{
 				Name:        "key1",
 				Aliases:     []string{"k1"},
 				Usage:       "encryption key with index 1",
 				Destination: &keys[1],
-				Required:    true,
+			},
+			&cli.StringFlag{
+				Name:        "key-file",
+				Aliases:     []string{"kf"},
+				Usage:       "provide encryption keys by yaml file",
+				Destination: &keyFilePath,
 			},
 			&cli.StringFlag{
 				Name:        "output",
@@ -108,4 +123,25 @@ func enc(filePaths []string, keys []string, output string) error {
 		return fmt.Errorf("failed to cipher to file: %w", err)
 	}
 	return nil
+}
+
+type KeyFile struct {
+	Keys []string `yaml:"keys"`
+}
+
+func getKeysFromKeyFile(keyFilePath string) ([]string, error) {
+	f, err := os.Open(keyFilePath)
+	if err != nil {
+		return nil, fmt.Errorf("failed to open key file: %w", err)
+	}
+	b, err := ioutil.ReadAll(f)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read key file: %w", err)
+	}
+	kf := KeyFile{}
+	err = yaml.Unmarshal(b, &kf)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read key file: %w", err)
+	}
+	return kf.Keys, nil
 }
